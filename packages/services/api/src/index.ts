1bc58ddf9c647329012dac5dbf8d8a353c13fc0a
Dargon789 <64915515+Dargon789@users.noreply.github.com>
2026-08-14 08:07:06 +0700

    0x v2 (#737)
    * provider: eip-5792 support
    
    * provider: use eip-5792
    
    * eip-7795
    
    * stack@eip-7795 bindings
    
    * fixup! eip-7795
    
    * Update packages/relayer/src/precondition.ts
    
    Co-authored-by: sourcery-ai[bot] <58596630+sourcery-ai[bot]@users.noreply.github.com>
    Signed-off-by: Dargon789 <64915515+Dargon789@users.noreply.github.com>
    
    ---------
    
    Signed-off-by: Dargon789 <64915515+Dargon789@users.noreply.github.com>
    Co-authored-by: William Hua <william@attente.ca>
    Co-authored-by: googleworkspace-bot <googleworkspace-bot@google.com>
    Co-authored-by: sourcery-ai[bot] <58596630+sourcery-ai[bot]@users.noreply.github.com>

--- packages/services/api/src/index.ts
+++ packages/services/api/src/index.ts
@@ -1,12 +1,17 @@
-export * from './api.gen.js'
+import {
+  Precondition as ChainPrecondition,
+  encodePrecondition as encodeChainPrecondition,
+  isPrecondition as isChainPrecondition
+} from '@0xsequence/relayer'
+import { ethers } from 'ethers'
 
-import { API as ApiRpc } from './api.gen.js'
+import * as proto from './api.gen'
 
-export class SequenceAPIClient extends ApiRpc {
+export class SequenceAPIClient extends proto.API {
   constructor(
     hostname: string,
     public projectAccessKey?: string,
-    public jwtAuth?: string,
+    public jwtAuth?: string
   ) {
     super(hostname.endsWith('/') ? hostname.slice(0, -1) : hostname, fetch)
     this.fetch = this._fetch
@@ -15,7 +20,7 @@ export class SequenceAPIClient extends ApiRpc {
   _fetch = (input: RequestInfo, init?: RequestInit): Promise<Response> => {
     // automatically include jwt and access key auth header to requests
     // if its been set on the api client
-    const headers: Record<string, string> = {}
+    const headers: { [key: string]: any } = {}
 
     const jwtAuth = this.jwtAuth
     const projectAccessKey = this.projectAccessKey
@@ -33,4 +38,35 @@ export class SequenceAPIClient extends ApiRpc {
 
     return fetch(input, init)
   }
+}
+
+export * from './api.gen'
+
+export type Precondition = { chainId: ethers.BigNumberish } & ChainPrecondition
+
+export function isPrecondition(precondition: any): precondition is Precondition {
+  return (
+    typeof precondition === 'object' && precondition && isBigNumberish(precondition.chainId) && isChainPrecondition(precondition)
+  )
+}
+
+export function encodePrecondition(precondition: Precondition): proto.Precondition {
+  const { type, precondition: args } = encodeChainPrecondition(precondition)
+  delete args.chainId
+  return { type, chainID: encodeBigNumberish(precondition.chainId), precondition: args }
+}
+
+function isBigNumberish(value: any): value is ethers.BigNumberish {
+  try {
+    ethers.toBigInt(value)
+    return true
+  } catch {
+    return false
+  }
+}
+
+function encodeBigNumberish<T extends ethers.BigNumberish | undefined>(
+  value: T
+): T extends ethers.BigNumberish ? string : undefined {
+  return value !== undefined ? ethers.toBigInt(value).toString() : (undefined as any)
 }
